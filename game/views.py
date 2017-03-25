c180582fcb6fcb8dd36c0168966fb4a772b1b3de
User Termotronika <termotronika@s11.mydevil.net>
2017-03-25 18:54:10 +0100

    Add tie

--- game/views.py
+++ game/views.py
@@ -16,34 +16,9 @@ def play(request, player_id, gesture):
     game = Game.objects.last()
     if ((game.gesture_1 == "rock" and game.gesture_2 == "paper") or (game.gesture_1 == "scisors" and game.gesture_2 == "rock" ) or (game.gesture_1 == "paper" and game.gesture_2 == "scisors")):
         winer = game.id_player_2
+    elif (game.gesture_1 == game.gesture_2):
+        winer = "tie"
     else:
         winer = game.id_player_1
     Game.objects.all().update(pending_request=False)
     return HttpResponse(json.dumps(winer), content_type="application/json")
-
-def play_a(request, gesture):
-    player_id = 1
-    game = GameManager().create_game(player_id,gesture)
-    if (game.pending_request == True):
-        time.sleep(10)
-    game = Game.objects.last()
-    if ((game.gesture_1 == "rock" and game.gesture_2 == "paper") or (game.gesture_1 == "scisors" and game.gesture_2 == "rock" ) or (game.gesture_1 == "paper" and game.gesture_2 == "scisors")):
-        winer = game.id_player_2
-    else:
-        winer = game.id_player_1
-    Game.objects.all().update(pending_request=False)
-    return HttpResponse(json.dumps(winer), content_type="application/json")
-
-def play_b(request, gesture):
-    player_id = 2
-    game = GameManager().create_game(player_id,gesture)
-    if (game.pending_request == True):
-        time.sleep(10)
-    game = Game.objects.last()
-    if ((game.gesture_1 == "rock" and game.gesture_2 == "paper") or (game.gesture_1 == "scisors" and game.gesture_2 == "rock" ) or (game.gesture_1 == "paper" and game.gesture_2 == "scisors")):
-        winer = game.id_player_2
-    else:
-        winer = game.id_player_1
-    Game.objects.all().update(pending_request=False)
-    return HttpResponse(json.dumps(winer), content_type="application/json")
-
